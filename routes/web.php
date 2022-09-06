999fa77bc466006967cc668e79614a9e8e89b5fb
danielbagus <=>
2022-09-06 11:12:21 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', function () {
+    return ('Selamat Datang');
+});
+
+Route::get('/about', function () {
+    return ('NIM: 2241727042<br>Nama : Daniel Bagus Christyanto<br>Kelas : Web-A');
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return  'Halaman artikel ini dengan id ' . $id;
+});
